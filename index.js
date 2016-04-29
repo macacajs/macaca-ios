ab0599fbd9cbff2a412085c3256970c50c2c05c5
zenzhu <zic.zhu@gmail.com>
2016-04-29 13:54:58 +0800

    Clear comments

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/* ================================================================
- * macaca-ios by xdf(xudafeng[at]126.com)
- *
- * first created at : Thu Dec 24 2015 18:47:25 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright  xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
 module.exports = require('./lib/macaca-ios');
--- test/macaca-ios.test.js
+++ test/macaca-ios.test.js
@@ -1,19 +1,6 @@
-/* ================================================================
- * macaca-ios by xdf(xudafeng[at]126.com)
- *
- * first created at : Thu Dec 24 2015 18:47:25 GMT+0800 (CST)
- *
- * ================================================================
- * Copyright 2013 xdf
- *
- * Licensed under the MIT License
- * You may not use this file except in compliance with the License.
- *
- * ================================================================ */
-
 'use strict';
 
-var IOS = require('..');
+var IOS = require('../lib/macaca-ios');
 
 describe('test', function() {
   it('should be ok', function() {
